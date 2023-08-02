cac193c8631a147e58e9f5c621c5cfb764cf20df
Hieu Nguyen <hieunhd0@gmail.com>
2023-08-02 20:03:34 +0700

    Initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import vueIcon from "@iconify/icons-logos/vue";
+import awsIcon from '@iconify/icons-logos/aws';
+import kubernetesIcon from '@iconify/icons-logos/kubernetes';
+import pythonIcon from '@iconify/icons-logos/python';
+
 
 class About extends Component {
   render() {
@@ -31,16 +32,22 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
+                    icon={awsIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    width="48"
+                    height="48"
                   />
                   <Icon
-                    icon={reactIcon}
+                    icon={kubernetesIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    width="48"
+                    height="48"
                   />
                   <Icon
-                    icon={vueIcon}
+                    icon={pythonIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    width="48"
+                    height="48"
                   />
                 </span>
               </div>
